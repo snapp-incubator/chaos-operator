5488c88c6f993cf5a9064eb71fccea8faa7a9277
Anis <anis@Aniss-MacBook-Air.local>
2024-02-28 17:13:16 +0330

    add kubebuilder rbac for pod resource

--- internal/controller/networkchaos_controller.go
+++ internal/controller/networkchaos_controller.go
@@ -64,6 +64,9 @@ const (
 //+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=services/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups="",resources=services/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=pods/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups="",resources=pods/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
